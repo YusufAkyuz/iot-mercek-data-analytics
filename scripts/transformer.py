2eb9849c19ea316a822a6b8b542502d58a573849
Yusuf Akyuz <yusufakyus47@gmail.com>
2025-05-08 14:37:11 +0300

    Transformer were included in the project.

---
+++ scripts/transformer.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from scripts.mappers import *
+
+mapper_funcs = {
+    "getBitwiseEnum": get_bitwise_enum,
+    "getValue": get_value,
+    "calcVoltage": calc_voltage,
+    "calcSpin": calc_spin,
+    "getEnum": get_enum,
+    "getEnumOrDefault": get_enum_or_default,
+    "calcChildLock": calc_child_lock,
+    "getAsMin": get_as_min,
+}
+
+
+def process_log(log_arr, config):
+    result = {}
+    for key, meta in config.items():
+        info = meta["modelInfo"]
+        indexes = info["index"]
+        mapper = meta["mapperFunc"]
+        source_type = meta["sourceType"]
+        values = [log_arr[i] for i in indexes]
+        func = mapper_funcs.get(mapper)
+        if func:
+            result[key] = func(values if len(values) > 1 else values[0], info.get("bitSetEnumValues", info.get("enumValues", {})))
+    return result
