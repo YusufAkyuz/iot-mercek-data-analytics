f3415d96976688afde06007b1aa2be484cab4491
Yusuf Akyuz <yusufakyus47@gmail.com>
2025-05-08 13:48:00 +0300

    Configuration loader class has been completed

---
+++ scripts/config_loader.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+# İlk olarak varolan config.json dosyasını kontrol edip python objesi haline çevirme işlemi gerçekleştirilecek.
+def load_config(config_path):
+    if os.path.exists(config_path):
+        with open(config_path, 'r') as file:
+            try:
+                config = json.load(file)
+                return config
+            except json.JSONDecodeError as e:
+                print(f"JSON Decode Error: {e}")
+                return None
+    else:
+        print("File path not found:", config_path)
+        return None
+
+config = load_config('./config/v1_10.json')
+print(config)
