25fc007890edf40fd9fae884ce29427070312eb4
Yusuf Akyuz <yusufakyus47@gmail.com>
2025-05-10 15:41:47 +0300

    The feature of getting parameters from the terminal with argparse was added to the project.

--- scripts/test.py
+++ scripts/test.py
@@ -3,6 +3,7 @@ import pandas as pd
 import re
 from config_loader import load_config
 from transformer import process_log
+import argparse
 
 def extract_log_data(raw_segments):
     try:
@@ -28,53 +29,32 @@ def extract_log_data(raw_segments):
         return [], "error"
 
 def main():
-    config = load_config('./config/v1_10.json')
+    parser = argparse.ArgumentParser(description="IoT Mercek Data Interpreter")
+    parser.add_argument('--config', type=str, default='config/v1_10.json', help='Path to configuration file')
+    parser.add_argument('--input', type=str, default='data/1.txt', help='Path to input data file')
+    parser.add_argument('--output', type=str, default='output/processed_data.csv', help='Path to output CSV file')
+
+    args = parser.parse_args()
+
+    config = load_config(args.config)
 
     if config is None:
         print("The configuration file was not found.")
         return
 
-    input_file = './data/1.txt'
-    output_file = './output/processed_data.csv'
+    results = []
 
-    all_results = []
-
-    with open(input_file, "r", encoding="utf-8") as f:
+    with open(args.input, "r", encoding="utf-8") as f:
         for line in f:
-            try:
-                parts = line.strip().split(";")
-                if len(parts) < 5:
-                    continue
+            parts = line.strip().split(";")
+            log_arr, conn_state = extract_log_data(parts)
+            parsed = process_log(log_arr, config)
+            parsed["connState"] = conn_state
+            results.append(parsed)
 
-                # ilk 4 parça appliance_id, lat, lon ve timestamp
-                appliance_id = parts[0]
-                lat = float(parts[1])
-                lon = float(parts[2])
-                try:
-                    ts = int(parts[3])
-                except Exception as e:
-                    print(f"Timestamp conversion error: {parts[3]} is not a valid timestamp.")
-    
-                # Geri kalan parçalar log verisi bunları düzenlemek ve işlemek için extract_log_data fonksiyonu çağrılıyor.
-                log_arr, conn_state = extract_log_data(parts[4:])
-
-                result = process_log(log_arr, config)
-                result.update({
-                    "ApplianceId": appliance_id,
-                    "Latitude": lat,
-                    "Longitude": lon,
-                    "Timestamp": ts,
-                    "ConnState": conn_state
-                })
-                all_results.append(result)
-            except Exception as e:
-                print(f"The row could not be processed: {e}")
-                continue
-
-    # Olusan çıktıları csv de kaydetme
-    df = pd.DataFrame(all_results)
-    df.to_csv(output_file, index=False, encoding="utf-8-sig")
-    print(f"{len(all_results)} record was successfully processed and saved to file '{output_file}'.")
+    df = pd.DataFrame(results)
+    df.to_csv(args.output, index=False)
+    print(f"Data has been written to {args.output}")
 
 if __name__ == "__main__":
     main()
